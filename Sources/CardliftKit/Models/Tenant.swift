d9f20ad5332288e23befb9c3d8cf0a39f93f4225
Ray Nirola <ray@nirola.in>
2025-01-11 23:23:44 +0530

    use remote data to build the upsell ui

--- Sources/CardliftKit/Models/Tenant.swift
+++ Sources/CardliftKit/Models/Tenant.swift
@@ -9,37 +9,15 @@ import Foundation
 
 
 struct Tenant: Codable, Identifiable {
-    let id, slug: String
-    let name, card: String
-    // let foreground_color, background_color: String
-    // let app_store_url: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, slug, name, card
-    }
+    let id, slug, name, bundleIdentifier: String
+    let appStoreLink: String
+    let cardImage: String
+    let foregroundColor, backgroundColor: ConfigColor
+    let buttonRadius: Double
+    let upsellLabel, title: String
+    let features: [String]
 }
 
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-            return true
-    }
-
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(0)
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-            let container = try decoder.singleValueContainer()
-            if !container.decodeNil() {
-                    throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-            }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-            var container = encoder.singleValueContainer()
-            try container.encodeNil()
-    }
+struct ConfigColor: Codable {
+    let r, g, b: Double
 }
--- Sources/CardliftKit/Upsell.swift
+++ Sources/CardliftKit/Upsell.swift
@@ -2,29 +2,29 @@ import SwiftUI
 
 /**
  A view model that fetches and holds tenant data.
-`TenantViewModel` is an observable object that fetches tenant data from a remote server and publishes it.
+ `TenantViewModel` is an observable object that fetches tenant data from a remote server and publishes it.
  - Properties:
-    - tenant: The tenant data fetched from the server. It is an optional `Tenant` object.
+ - tenant: The tenant data fetched from the server. It is an optional `Tenant` object.
  - Methods:
-    - fetchTenant(): Fetches tenant data from a predefined URL and updates the `tenant` property. Handles network errors and JSON decoding errors.
-*/
+ - fetchTenant(): Fetches tenant data from a predefined URL and updates the `tenant` property. Handles network errors and JSON decoding errors.
+ */
 public class TenantViewModel: ObservableObject {
     @Published var tenant: Tenant?
-
+    
     func fetchTenant(slug: String = "acme") {
-        let url = URL(string: "https://api.cardlift.co/v1/tenants/\(slug)")!
-
+        let url = URL(string: "https://api.cardlift.co/v1/tenants/\(slug)/ios")!
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("Network error: \(error.localizedDescription)")
                 return
             }
-
+            
             guard let data = data else {
                 print("No data returned from server")
                 return
             }
-
+            
             let decoder = JSONDecoder()
             do {
                 let tenant = try decoder.decode(Tenant.self, from: data)
@@ -52,12 +52,12 @@ public struct Upsell: View {
     @StateObject var tenantViewModel = TenantViewModel()
     var slug: String
     var buttonConfig: UpsellButtonConfig
-
+    
     public init( slug: String, buttonConfig: UpsellButtonConfig) {
         self.slug = slug
         self.buttonConfig = buttonConfig
     }
-
+    
     public var body: some View {
         Button(action: {
             self.showSheet.toggle()
@@ -85,18 +85,18 @@ public struct Upsell: View {
         .onAppear {
             tenantViewModel.fetchTenant(slug: slug)
         }
-
+        
     }
-
+    
     struct UpsellSheet: View {
         @Environment(\.openURL) var openURL
         @EnvironmentObject var tenantViewModel: TenantViewModel
-
+        
         var body: some View {
             VStack {
                 Spacer()
-
-                Text("Simplify your checkout with our Safari Extension")
+                
+                Text(tenantViewModel.tenant!.title)
                     .foregroundColor(Color(red: 55/255, green: 55/255, blue: 55/255))
                     .font(.system(size: 32, weight: .bold))
                     .tracking(0.3)
@@ -104,8 +104,8 @@ public struct Upsell: View {
                     .multilineTextAlignment(.center)
                     .frame(maxWidth: 300, alignment: .center)
                     .padding()
-
-                AsyncImage(url: URL(string: tenantViewModel.tenant?.card ?? "")) { image in
+                
+                AsyncImage(url: URL(string: tenantViewModel.tenant?.cardImage ?? "")) { image in
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -116,59 +116,40 @@ public struct Upsell: View {
                         .frame(width: UIScreen.main.bounds.width - 80, height: 190)
                         .padding()
                 }
-
+                
                 Group {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(Color(red: 110/255, green: 100/255, blue: 100/255))
-                            .frame(width: 4, height: 4)
-                        Text("Checkout 2x quicker")
-                            .font(.system(size: 16, weight: .medium))
-                            .padding(.leading, 6)
-                            .foregroundColor(Color(red: 140/255, green: 140/255, blue: 140/255))
-                        Spacer()
-                    }.padding(.top, 12)
-                    HStack {
-                        Spacer()
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(Color(red: 110/255, green: 100/255, blue: 100/255))
-                            .frame(width: 4, height: 4)
-                        Text("Change your card at frequent sites")
-                            .font(.system(size: 16, weight: .medium))
-                            .padding(.leading, 6)
-                            .foregroundColor(Color(red: 140/255, green: 140/255, blue: 140/255))
-                        Spacer()
-                    }
-                    HStack {
-                        Spacer()
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(Color(red: 110/255, green: 100/255, blue: 100/255))
-                            .frame(width: 4, height: 4)
-                        Text("Earn more points by using this card more")
-                            .font(.system(size: 16, weight: .medium))
-                            .padding(.leading, 6)
-                            .foregroundColor(Color(red: 140/255, green: 140/255, blue: 140/255))
-
-                        Spacer()
+                    ForEach(tenantViewModel.tenant!.features, id: \.self) { feature in
+                        HStack {
+                            Spacer()
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(Color(red: 110/255, green: 100/255, blue: 100/255))
+                                .frame(width: 4, height: 4)
+                            Text(feature)
+                                .font(.system(size: 16, weight: .medium))
+                                .padding(.leading, 6)
+                                .foregroundColor(Color(red: 140/255, green: 140/255, blue: 140/255))
+                            Spacer()
+                        }
                     }
                 }
-
+                
                 Spacer()
-
+                
                 VStack {
                     Button(action: {
-                        openURL(URL(string:  "")!)
+                        openURL(URL(string: tenantViewModel.tenant!.appStoreLink)!)
                     }) {
-                        Text("Add \(tenantViewModel.tenant?.name ?? "") to your wallet")
+                        let bGColor = tenantViewModel.tenant!.backgroundColor
+                        
+                        Text("Add \(tenantViewModel.tenant!.name) to your wallet")
                             .font(.system(size: 20, weight: .semibold))
                             .padding()
                             .frame(maxWidth: UIScreen.main.bounds.width - 40)
-                            .background(Color(red: 218/255, green: 41/255, blue: 36/255))
+                            .background(Color(red: Double(bGColor.r/255), green: bGColor.g/255, blue: bGColor.b/255))
                             .foregroundColor(.white)
-                            .cornerRadius(40)
+                            .cornerRadius(tenantViewModel.tenant!.buttonRadius)
                     }
-
+                    
                     HStack {
                         Spacer()
                         Image(systemName: "lock.fill")
@@ -180,7 +161,7 @@ public struct Upsell: View {
                         Spacer()
                     }.padding(.top, 4)
                 }
-
+                
             }
             .padding()
         }
@@ -192,14 +173,14 @@ public struct Upsell: View {
 /**
  A view that displays a shimmering effect using a rounded rectangle.
  The shimmer effect animates the opacity of the rectangle between 0.1 and 0.5.
-
+ 
  The `Shimmer` view uses a `State` property `isAnimating` to control the animation.
  When the view appears, it starts an infinite linear animation that toggles the `isAnimating`
  state, creating a shimmering effect.
-*/
+ */
 struct Shimmer: View {
     @State private var isAnimating = false
-
+    
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
             .fill(Color(UIColor.lightGray))
